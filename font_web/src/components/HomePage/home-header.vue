c60d2148b320ac7c0dd134432d3d84a9a0b3e7cb
xzk <2587579585@qq.com>
2025-01-22 16:13:50 +0800

    init

--- font_web/src/components/HomePage/home-header.vue
+++ font_web/src/components/HomePage/home-header.vue
@@ -12,9 +12,19 @@ function onSubmit() {
   SearchKeyword.updateKeyword(searchKeyWord.value)
   router.push('search')
 }
+let threm = "light"
+function changeThrme() {
+  if(threm == "light"){
+    threm = "dark"
+    document.body.setAttribute('arco-theme', 'dark')
+  }else{
+    threm = "light"
+    document.body.removeAttribute('arco-theme');
+  }
+}
 </script>
 <template>
-  <div class="fixed top-0 w-full h-70px bg-#c91317 z-100 flex items-center justify-between">
+  <div class="test fixed top-0 w-full h-70px z-100 bg-bgc flex items-center justify-between">
     <div class="flex items-center">
       <!-- 网站名网站logo -->
       <!-- 头部导航栏 左侧 logo-->
@@ -24,58 +34,12 @@ function onSubmit() {
         </div>
         <div class="pl-20px text-20px color-#fff">Insomnia-music</div>
       </div>
-      <!-- 搜索栏 -->
-      <!-- 搜索 -->
-      <div class="h-50px relative">
-        <!-- 搜索栏 vant -->
-        <van-search
-          v-model="searchKeyWord"
-          @focus="SearchFocus"
-          @search="onSubmit"
-          placeholder="请输入搜索关键词"
-          background="#c61216"
-          shape="round"
-        />
-        <!-- 热搜bang -->
-        <!-- <div class="hotSearch" v-if="!searchSuggestList">
-          <div class="hotSearchTitle">热搜榜</div>
-            <div class="hotSearchItem"
-                 v-for="(item, index) in hotSearchList"
-                 :key="index"
-                 @click="clickHotSearchItem(item.searchWord)">
-              <div class="hotSearchIndex"
-                   :class="index < 3 ? 'topThree' : ''">
-                {{ index + 1 }}
-              </div>
-              <div class="hotSearchInfo">
-                <div class="hotSearchWord"
-                     :class="index < 3 ? 'hotSearchWordTopThree' : ''">
-                  {{ item.searchWord }}
-                </div>
-                <div class="hotSearchContent">{{ item.content }}</div>
-              </div>
-            </div>
-        </div> -->
-      </div>
     </div>
-    <!-- 登录框 -->
-    <div class="w-200px">
-      <!-- 未登录 登录按钮  -->
-      <div class="flex items-center">
-        <a-popover trigger="click" :popup-visible="popupSate">
-          <img
-            class="w-50px h-50px rounded-full bg-black"
-            src="http://p1.music.126.net/t292NH_CRFn9tiQHIfQLNQ==/109951169966219680.jpg
-"
-            alt=""
-          />
-          <template #content>
-
-          </template>
-        </a-popover>
-        <div class="ml-20px">点击头像登录</div>
-      </div>
-      <!-- 登录状态  用户头像 用户名称 -->
-    </div>
+    <a-button @click="changeThrme">主题</a-button>
   </div>
 </template>
+<style>
+    /* .test{
+      background-color:var(--color-bg-2);
+    } */
+  </style>
---
+++ font_web/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        // 在这里加入你的 CSS 变量
+        colors: {
+          'bgc': 'var(--color-bg-1)',
+        }
+      }
+    }
+  }
